5854e004eef66442f13c1002b8292e417390dd6d
Ulises Vallejo <jose.vallejo7479@alumnos.udg.mx>
2020-09-19 13:19:42 -0500

    Aagregamos y mostramos personajes con un encabezado .h

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "personaje.h"
 
 void capturar_enteros(){
     int enteros[5];
@@ -40,6 +41,7 @@ int main(){
         printf("1. Capturar enteros\n");
         printf("2. Mostrar cadena n veces\n");
         printf("3. Agregar personaje\n");
+        printf("4. Mostrar personajes\n");
         printf("0. Salir\n");
         scanf("%c",&op);
 
@@ -57,9 +59,11 @@ int main(){
             mostrar(n, cadena);
             break;
         case '3':
-
+            capturar_persona();
             break;
-        
+        case '4':
+            mostrar_personajes();
+            break;
         default:
             break;
         }
